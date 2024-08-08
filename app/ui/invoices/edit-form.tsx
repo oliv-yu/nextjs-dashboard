01940922838b78d28edfef4a51a819eb933eb3a1
Olivia Yu <olivia.yu@liferay.com>
2024-08-08 16:35:45 -0700

    Mutate data by add, update, delete actions (chap 12)

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +18,15 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form>
+    // passing id into updateInvoice function will NOT work
+    // use bind to pass the id as the first argument
+
+    // Passing in a hidden input value will also work, but it's not recommended
+    // for sensitive data like an invoice ID
+
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
