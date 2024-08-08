4cf2fed30c1728a6dc43090c6952a636ba73ef9e
Olivia Yu <olivia.yu@liferay.com>
2024-08-08 14:57:55 -0700

    Add support for partial prerendering (chap 10)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Partial Pre-rendering - defer rendering parts of your application
+  // until some condition is met (e.g. data is loaded).
+  // This 'incremental' allows adopting PPR for specific routes.
+  //
+  // Property is commented out due to error:
+  // The experimental.ppr preview feature can only be enabled when using
+  // the latest canary version of Next.js.
+  // See more info here: https://nextjs.org/docs/messages/ppr-preview`);
+
+  experimental: {
+    //ppr: 'incremental',
+  },
+};
 
 module.exports = nextConfig;
